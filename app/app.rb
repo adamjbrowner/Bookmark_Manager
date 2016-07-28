54bb732b28b0c9d34d87828ed88e8c37cd7d6d15
Adam <adamjbrowner@gmail.com>
2016-07-28 22:32:38 +0100

    final commit of the day, reset to before heroku test

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,7 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
-#require 'sinatra/flash'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
